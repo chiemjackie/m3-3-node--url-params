cfff91cad8e76647110f9bcdefe4321281d93494
Jackie Chiem <32667298+chiemjackie@users.noreply.github.com>
2020-07-07 00:40:22 -0400

    completed 1.1-1.4, and 1.6, started 1.7 and 1.8

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const morgan = require('morgan');
+const express = require('express');
 
 const { top50 } = require('./data/top50');
 
@@ -13,15 +14,38 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.set('view engine', 'ejs');
 
+const title = (req, res) => {
+    res.render('pages/top50', {
+        title: 'Top 50 Songs Streamed on Spotify',
+        top50: top50,
+    })
+}
+
 // endpoints here
+app.get('/top50', title);
 
-// handle 404s
-app.git('*', (req, res) => {
-    res.status(404);
-    res.render('pages/fourOhFour', {
-        title: 'I got nothing',
-        path: req.originalUrl
-    });
-});
+const pageLoad = () => {
+    if (top50.find(song => song.rank <= 50)) {
+        app.get('/top50/song/:currentSong', (req, res) => {
+            let songRank = req.params.currentSong;
+            const song = top50.find(song => song.rank == songRank)
+            res.render('pages/songPage', {
+                content: song,
+                title: `Song #${songRank}`
+            })
+        });
+    } else {
+    // handle 404s
+        app.get('*', (req, res) => {
+            res.status(404);
+            res.render('pages/fourOhFour', {
+                title: 'I got nothing',
+                path: req.originalUrl
+            });
+        });
+    }
+}
 
-get.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+pageLoad();
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
